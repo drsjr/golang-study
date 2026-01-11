d09d2ca3035e201108af84297f303ebb32fed54d
Djalma Junior <djalma.rsjr@gmail.com>
2026-01-11 10:33:24 -0300

    study chapter 1

---
+++ chapter_1/dup.go
@@ -0,0 +1,82 @@
+package main
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"io/ioutil"
+)
+
+func main() {
+	// dup1()
+	// dup2()
+	dup3()
+}
+
+func dup1() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	// nota: ignora erros em potencial do input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("(dup2)count: %d\t|value: %s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	//nota: ignorand erros em potencial de input.Err()
+}
+
+func dup3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("(dup3)count: %d\t|value: %s\n", n, line)
+		}
+	}
+}
